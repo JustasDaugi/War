11a7c45bd56c02f2fb72c8ff9dff6cb26fee970f
Justas Daugininkaitis <justas.daugininkaitis@gmail.com>
2024-03-14 18:21:33 +0200

    Implemented class Deck

--- war.py
+++ war.py
@@ -1,3 +1,5 @@
+import random
+
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
@@ -5,5 +7,24 @@ class Card:
 
     def __str__(self):
         return f"{self.rank} of {self.suit}"
-    
-print(Card(2, "Hearts"))
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(rank, suit))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def __str__(self):
+        deck_str = '.'.join([str(card) for card in self.cards])
+        return f"Deck: [{deck_str}]"
+
+deck = Deck()
+print(deck)
+
